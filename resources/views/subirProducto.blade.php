dc803af8a696556e359c5611a92321046ba112b5
Manuel Cruz <cruzmanuelar@gmail.com>
2020-11-03 19:17:27 -0500

    fecha

--- resources/views/subirProducto.blade.php
+++ resources/views/subirProducto.blade.php
@@ -30,26 +30,26 @@
             <div class="col-12">
                 <form>
                     <div class="form-row">
-                      <div class="form-group col-md-4">
-                        <label for="inputEmail4">Nombre</label>
-                        <input type="text" class="form-control" id="">
-                      </div>
-                      <div class="form-group col-md-4">
-                        <label for="inputPassword4">Categoría</label>
-                        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                            <option selected>Computador</option>
-                            <option value="1">Ropa</option>
-                            <option value="1">Electrodoméstico</option>
-                            <option value="1">Libro</option>
-                        </select>
-                      </div>
-                      <div class="form-group col-md-4">
-                        <label for="">Estado</label>
-                        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                            <option selected>Nuevo</option>
-                            <option value="1">Usado</option>
-                        </select>
-                      </div>
+                        <div class="form-group col-md-4">
+                            <label for="inputEmail4">Nombre</label>
+                            <input type="text" class="form-control" id="">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="inputPassword4">Categoría</label>
+                            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                <option selected>Computador</option>
+                                <option value="1">Ropa</option>
+                                <option value="1">Electrodoméstico</option>
+                                <option value="1">Libro</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="">Estado</label>
+                            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                <option selected>Nuevo</option>
+                                <option value="1">Usado</option>
+                            </select>
+                        </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-6">
@@ -65,12 +65,12 @@
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-4">
-                          <label for="">Devoluciones</label>
-                          <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                            <option selected>Sí</option>
-                            <option value="1">No</option>
-                            <option value="1">Negociable</option>
-                        </select>
+                            <label for="">Devoluciones</label>
+                            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                <option selected>Sí</option>
+                                <option value="1">No</option>
+                                <option value="1">Negociable</option>
+                            </select>
                         </div>
                         <div class="form-group col-md-4">
                           <label for="">Precio inicial</label>
@@ -78,18 +78,23 @@
                         </div>
                         <div class="form-group col-md-4">
                             <label for="">Fecha Subasta</label>
-                            <input type="text" class="form-control" id="">
+                            <input class="form-control" type="datetime-local" value="2020-08-19T13:45:00" id="example-datetime-local-input">
                         </div>
-                      </div>
-                    <div class="form-group">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                          Aceptar términos y condiciones
-                        </label>
-                      </div>
                     </div>
-                    <button type="submit" class="btn btn-success">Subastar producto</button>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" id="gridCheck">
+                              <label class="form-check-label" for="gridCheck">
+                                Aceptar términos y condiciones
+                              </label>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-5">
+                            <button type="submit" class="btn btn-block btn-success">Subastar producto</button>
+                            </div>
+                        </div>
+                    </div>
                   </form>
             </div>
         </div>
