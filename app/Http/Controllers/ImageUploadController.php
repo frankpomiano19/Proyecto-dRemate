9fd21c207bf09358e405a0bf8e71bbf017f61440
Manuel Cruz <cruzmanuelar@gmail.com>
2020-11-21 22:57:02 -0500

    Controlador Imagen

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -1,10 +1,30 @@
 <?php
 
+use JD\Cloudder\Facades\Cloudder;
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class ImageUploadController extends Controller
 {
-    //
+    public function home()
+   {
+       return view('home2');
+   }
+
+   public function uploadImages(Request $request)
+   {
+       $this->validate($request,[
+           'image_name'=>'required|mimes:jpeg,bmp,jpg,png|between:1, 6000',
+       ]);
+
+       $image_name = $request->file('image_name')->getRealPath();;
+
+       Cloudder::upload($image_name, null);
+
+       return redirect()->back()->with('status', 'Image Uploaded Successfully');
+
+   }
+
+}
 }
