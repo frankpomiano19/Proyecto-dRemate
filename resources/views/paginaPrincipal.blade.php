455552ef68d88c3fe27f7f564ea8d49fbbe385c2
Rodrigo_Prado123 <rodrigopradoc1998@gmail.com>
2020-11-24 13:39:08 -0500

    home version martes

--- resources/views/paginaPrincipal.blade.php
+++ resources/views/paginaPrincipal.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('cont_cabe')
-    <title>Home - dRemate</title>
+    <title>Home - D'REMATE</title>
 
 @endsection
 
@@ -11,7 +11,7 @@
 @endsection
 
 @section('contenidoCSS')
-    <!-- Colocar css-->
+    <!--footer.css pal footer / barra.css pal navbar / no es necesario el fontawesome-->
     <link rel="stylesheet" href="css/inicio.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/barra.css">
@@ -21,94 +21,137 @@
 
 @section('contenido')
 
-<!--header-->
-<div class="navbar-sticky bg-light">
-        <div class="navbar navbar-expand-lg navbar-light" style="background:#343a40!important;padding-top: 12px;padding-bottom: 12px;">
-          <div class="container"><a class="navbar-brand d-none d-sm-block mr-3 flex-shrink-0" href="index.html" style="min-width: 7rem;"><img width="142" src="img/logo.png" alt="Cartzilla" style="height: 55px;"></a><a class="navbar-brand d-sm-none mr-2" href="index.html" style="min-width: 4.625rem;"><img width="74" src="img/logo.png" alt="Cartzilla" style="height: 34px;min-width: 74px;"></a>
-            <div class="input-group-overlay d-none d-lg-flex mx-4">
-              <input class="form-control appended-form-control" type="text" placeholder="Buscar productos">
-              <div class="input-group-append-overlay"><span class="input-group-text"><i class="fa fa-search"></i></span></div>
-            </div>
-            <div class="navbar-toolbar d-flex flex-shrink-0 align-items-center">
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"><span class="navbar-toggler-icon"></span></button><a class="navbar-tool navbar-stuck-toggler" href="#"><span class="navbar-tool-tooltip">Expand menu</span>
-                <div class="navbar-tool-icon-box"><i class="navbar-tool-icon czi-menu"></i></div></a><a class="navbar-tool d-none d-lg-flex" href="#"><span class="navbar-tool-tooltip">Favoritos</span>
-                <div class="navbar-tool-icon-box"><i class="fa fa-heart"></i></div></a><a class="navbar-tool ml-1 ml-lg-0 mr-n1 mr-lg-2" href="#signin-modal" data-toggle="modal">
-                <div class="navbar-tool-icon-box"><i class="fa fa-user"></i></div>
-                <div class="navbar-tool-text ml-n3"><small>Iniciar sesión</small>Mi cuenta</div></a>
-              <div class="navbar-tool dropdown ml-3"><div class="navbar-tool-icon-box bg-secondary"><i class="fa fa-money"></i></i></div><div class="navbar-tool-text" style="padding-left: 0px;"><small>Mis créditos</small>$00.00</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="navbar navbar-expand-lg navbar-light navbar-stuck-menu mt-n2 pt-0 pb-2" style="padding-bottom: 0px!important;">
-          <div class="container">
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-              <div class="input-group-overlay d-lg-none my-3">
-                <div class="input-group-prepend-overlay"><span class="input-group-text"><i class="fa fa-search"></i></span></div>
-                <input class="form-control prepended-form-control" type="text" placeholder="Buscar productos">
-              </div>
-              <ul class="navbar-nav mega-nav pr-lg-2 mr-lg-2">
-                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle pl-0" href="/category" data-toggle="dropdown"><i class="fa fa-th mr-2"></i>Categorías</a>
-                  <ul class="dropdown-menu px-2 pl-0 pb-4">
+<!--barra navegación-->
+<div class="navbar-sticky bg-light fixed-top">
+    <div class="navbar navbar-expand-lg navbar-light" style="background:#343a40!important;padding-top: 12px;padding-bottom: 20px;">
+        <div class="container" style="padding-left: 0px;margin-left: 25px;margin-right: 25px;"><a class="navbar-brand d-none d-sm-block mr-3 flex-shrink-0" href="/" style="min-width: 7rem;"><img width="142" src="img/logo.png" alt="Cartzilla" style="height: 55px;"></a><a class="navbar-brand d-sm-none mr-2" href="/" style="min-width: 4.625rem;"><img width="74" src="img/logo.png" alt="Cartzilla" style="height: 34px;min-width: 74px;"></a>
+        <ul class="navbar-nav mega-nav d-none pr-lg-2 mr-lg-2" style="margin-left: 200px;">
+                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle pl-0" href="/category" data-toggle="dropdown"><i class="fa fa-th mr-2" style="color:#dee2e6;"></i>Categorías</a>
+                <ul class="dropdown-menu px-2 pl-0 pb-4">
                     <div class="d-flex flex-wrap flex-md-nowrap">
-                      <div class="mega-dropdown-column pt-4 px-3">
+                    <div class="mega-dropdown-column pt-4 px-3">
                         <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://elempresario.mx/sites/default/files/imagecache/nota_completa/moda.jpg" alt="Moda"></a>
                             <a href="/category"><h6 class="font-size-base mb-2">Moda</h6></a>
                         </div>
-                      </div>
-                      <div class="mega-dropdown-column pt-4 px-3">
+                    </div>
+                    <div class="mega-dropdown-column pt-4 px-3">
                         <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://myperuglobal.com/wp-content/uploads/2015/10/Untitled-design.png" alt="Tecno"></a>
                             <a href="/category"><h6 class="font-size-base mb-2">Tecnologia</h6></a>
                         </div>
-                      </div>
-                      <div class="mega-dropdown-column pt-4 px-3">
+                    </div>
+                    <div class="mega-dropdown-column pt-4 px-3">
                         <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://kasaniu.com/wp-content/uploads/2017/08/ScreenHunter_450-Aug.-13-17.43-1170x680.jpg" alt="Anti"></a>
                             <a href="/category"><h6 class="font-size-base mb-2">Antigüedades</h6></a>
                         </div>
-                      </div>
+                    </div>
                     </div>
                     <div class="d-flex flex-wrap flex-md-nowrap">
-                      <div class="mega-dropdown-column pt-4 px-3">
+                    <div class="mega-dropdown-column pt-4 px-3">
                         <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://www.engelglobal.com/fileadmin/master/Branchen/technical_moulding/TEC_Sport___Freizeit.jpg" alt="Depor"></a>
                             <a href="/category"><h6 class="font-size-base mb-2">Deportes</h6></a>
                         </div>
-                      </div>
-                      <div class="mega-dropdown-column pt-4 px-3">
+                    </div>
+                    <div class="mega-dropdown-column pt-4 px-3">
                         <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://cdn.shopify.com/s/files/1/0899/2262/articles/10-tiendas-de-decoraci-n-de-interiores-para-tu-hogar.jpg?v=1559339599" alt="Hogar"></a>
                             <a href="/category"><h6 class="font-size-base mb-2">Hogar</h6></a>
                         </div>
-                      </div>
-                      <div class="mega-dropdown-column pt-4 px-3">
+                    </div>
+                    <div class="mega-dropdown-column pt-4 px-3">
                         <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://us.emedemujer.com/wp-content/uploads/sites/3/2017/02/Jardiner%C3%ADa-b%C3%A1sica-lo-que-toda-newbie-debe-saber-770x512.jpg" alt="Jardi"></a>
                             <a href="/category"><h6 class="font-size-base mb-2">Jardineria</h6></a>
                         </div>
-                      </div>
+                    </div>
                     </div>
                     </ul>
                 </li>
-              </ul>
-              <ul class="navbar-nav">
-                <li class="nav-item active"><a class="nav-link" href="#">Home</a>
+            </ul>
+            <div class="input-group-overlay d-none d-lg-flex mx-4">
+            <input class="form-control appended-form-control" type="text" placeholder="Buscar productos">
+            <div class="input-group-append-overlay"><span class="input-group-text"><i class="fa fa-search"></i></span></div>
+            </div> 
+        
+            <ul class="navbar-nav d-none" style="align-items: center;">
+                <li class="nav-item"><a class="nav-link" href="/">Home</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="#">Subasta Rápida</a>
+                <li class="nav-item"><a class="nav-link" href="/subastaRapida">Subasta Rápida</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="#">Comentarios</a>
+                <li class="nav-item"><a class="nav-link" href="/register">Ingresar</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="#">Próximamente</a>
+                <li class="nav-item"><a class="nav-link" href="/register">Registrarse</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="#">Próximamente</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+            </ul>
+
+
+            
         </div>
-      </div>
-<!--header-->
+    </div>
+    <div class="navbar navbar-expand-lg navbar-light navbar-stuck-menu mt-n2 pt-0 pb-2" style="padding-bottom: 0px!important;">
+        <div class="container">
+        <a class="navbar-brand d-none d-sm-block mr-3 flex-shrink-0" href="/" style="min-width: 7rem;"><img width="142" src="img/logo.png" alt="Cartzilla" style="height: 55px;"></a><!--<a class="navbar-brand d-sm-none mr-2" href="/" style="min-width: 4.625rem;"><img width="74" src="img/logo.png" alt="Cartzilla" style="height: 34px;min-width: 74px;"></a>-->
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="input-group-overlay d-lg-none my-3">
+            <div class="input-group-prepend-overlay"><span class="input-group-text"><i class="fa fa-search"></i></span></div>
+            <input class="form-control prepended-form-control" type="text" placeholder="Buscar productos">
+            </div>
+            <ul class="navbar-nav mega-nav pr-lg-2 mr-lg-2">
+            <li class="nav-item dropdown"><a class="nav-link dropdown-toggle pl-0" href="/category" data-toggle="dropdown" style="color:#343a40;font-weight: bold;"><i class="fa fa-th mr-2"></i>Categorías</a>
+                <ul class="dropdown-menu px-2 pl-0 pb-4">
+                <div class="d-flex flex-wrap flex-md-nowrap">
+                    <div class="mega-dropdown-column pt-4 px-3">
+                    <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://elempresario.mx/sites/default/files/imagecache/nota_completa/moda.jpg" alt="Moda"></a>
+                        <a href="/category"><h6 class="font-size-base mb-2">Moda</h6></a>
+                    </div>
+                    </div>
+                    <div class="mega-dropdown-column pt-4 px-3">
+                    <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://myperuglobal.com/wp-content/uploads/2015/10/Untitled-design.png" alt="Tecno"></a>
+                        <a href="/category"><h6 class="font-size-base mb-2">Tecnologia</h6></a>
+                    </div>
+                    </div>
+                    <div class="mega-dropdown-column pt-4 px-3">
+                    <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://kasaniu.com/wp-content/uploads/2017/08/ScreenHunter_450-Aug.-13-17.43-1170x680.jpg" alt="Anti"></a>
+                        <a href="/category"><h6 class="font-size-base mb-2">Antigüedades</h6></a>
+                    </div>
+                    </div>
+                </div>
+                <div class="d-flex flex-wrap flex-md-nowrap">
+                    <div class="mega-dropdown-column pt-4 px-3">
+                    <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://www.engelglobal.com/fileadmin/master/Branchen/technical_moulding/TEC_Sport___Freizeit.jpg" alt="Depor"></a>
+                        <a href="/category"><h6 class="font-size-base mb-2">Deportes</h6></a>
+                    </div>
+                    </div>
+                    <div class="mega-dropdown-column pt-4 px-3">
+                    <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://cdn.shopify.com/s/files/1/0899/2262/articles/10-tiendas-de-decoraci-n-de-interiores-para-tu-hogar.jpg?v=1559339599" alt="Hogar"></a>
+                        <a href="/category"><h6 class="font-size-base mb-2">Hogar</h6></a>
+                    </div>
+                    </div>
+                    <div class="mega-dropdown-column pt-4 px-3">
+                    <div class="widget widget-links"><a class="d-block overflow-hidden rounded-lg mb-3" href="#"><img src="https://us.emedemujer.com/wp-content/uploads/sites/3/2017/02/Jardiner%C3%ADa-b%C3%A1sica-lo-que-toda-newbie-debe-saber-770x512.jpg" alt="Jardi"></a>
+                        <a href="/category"><h6 class="font-size-base mb-2">Jardineria</h6></a>
+                    </div>
+                    </div>
+                </div>
+                </ul>
+            </li>
+            </ul>
+            <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="/" style="color:#343a40;font-weight: bold;">Home</a>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="/subastaRapida" style="color:#343a40;font-weight: bold;">Subasta Rápida</a>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="/register">Registrarse</a>
+            </li>
+            </ul>
+        </div>
+        </div>
+    </div>
+</div>
+<br><br>
+<!--fin barra navegación-->
 
 
 
 
-<div class="c-hero">
+<div class="c-hero" style="padding-top:21px;">
     <div class="c-hero__center">
         <h1 class="c-hero__title">SUBASTAS ONLINE D'REMATE</h1>
         <h2 class="c-hero__subtitle">
@@ -585,20 +628,20 @@
 
 <footer id="footer">
     <div class="container">
-      <h3>dRemate</h3>
-      <p>ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+      <h3>D'REMATE</h3>
+      <p></p>
       <div class="social-links">
         <a class="btn btn-social-icon btn-twitter"><span class="fa fa-twitter"></span></a>
         <a class="btn btn-social-icon btn-facebook"><span class="fa fa-facebook"></span></a>
         <a class="btn btn-social-icon btn-instagram"><span class="fa fa-instagram"></span></a>
       </div>
       <div class="copyright">
-        © Copyright <strong><span>dRemate</span></strong> 2020 - 3030. All Rights Reserved
+        © Copyright <strong><span>D'REMATE</span></strong> 2020 - 3030. All Rights Reserved
       </div>
     </div>
   </footer>
 
-<!--Footer-->
+<!--fin Footer-->
 @endsection
 
 @section('contenidoJSabajo')
