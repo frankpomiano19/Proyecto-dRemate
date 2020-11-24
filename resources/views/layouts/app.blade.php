237488f5c8870deb09b45196fd8512ec9f453243
Lum73 <alexandra.gonzales1@unmsm.edu.pe>
2020-11-23 22:49:17 -0500

    modificar registercontroller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -129,10 +129,19 @@
                         </div>
                     </li>
                 -->
+                <div class="container">
+            <div class="row">
+                 <div class="col-md-3">
+                     @include('includes.menu')
+                 </div>
+                 <div class="col-md-9">
+                 @yield('content')
+                </div>
                 </ul>
             </div>
         </div>
     </nav>
+    
     <br>
 
 
