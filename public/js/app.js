95f0c9244e33d94de9155eddd02c84887d2f4aea
HuamanAngel <oxipusioxx12@gmail.com>
2020-12-27 01:01:11 -0500

    Corregido error de webpack

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\38_borrar\proyectoConfiguracion\Proyecto-dRemate\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\38_borrar\proyectoConfiguracion\Proyecto-dRemate\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\proyectoRealizadosN\Proyecto-dRemate\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\proyectoRealizadosN\Proyecto-dRemate\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
