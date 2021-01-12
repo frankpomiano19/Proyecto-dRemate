92294bbcf27a2f9aa59253ba25a993780b87bb09
HuamanAngel <oxipusioxx12@gmail.com>
2021-01-12 14:25:28 -0500

    Arreglado error de login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -58,7 +58,11 @@ class LoginController extends Controller
 
         if(Auth::attempt($credentials))
        {
+           if(Auth::user()->userReportUser !=null){
             $status = Auth::user()->userReportUser->count();
+           }else{
+            $status = 0;
+           }
            if ($status >= 30) {
                 //return back()->with('message','Su cuenta ha sido baneada')->with('typealert', 'danger');
                 //Si tiene más de 30 denuncias es redirigido al login
